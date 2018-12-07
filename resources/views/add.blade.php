979cf2f1b8b1767f5e732a25844f99d24f72848e
haritsfahlevi <haritswahyu@gmail.com>
2018-12-07 13:42:38 +0700

    update add view to display task input form

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
-//this will add laravel’s default navbar to your page
-
 @section('content')
+<div class="container">
+                <h2>Add New Task</h2>
+               
+<form method="POST" action="/task">
 
-//here goes your body content
+    <div class="form-group">
+        <textarea name="description" class="form-control"></textarea>  
+    </div>
 
+
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary">Add Task</button>
+    </div>
+{{ csrf_field() }}
+</form>
+
+
+</div>
 @endsection
